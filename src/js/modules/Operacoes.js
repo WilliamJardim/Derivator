31c32ccc837571002f85c58d546da59b30c5aadf
WilliamJardim <williamalvesjardim@gmail.com>
2024-05-28 09:59:28 -0300

    Novo commit

--- src/js/modules/Operacoes.js
+++ src/js/modules/Operacoes.js
@@ -1,3 +1,11 @@
+/**
+* File Name: codigo-principal.js
+* Author Name: William Alves Jardim
+* Author Email: williamalvesjardim@gmail.com
+*
+* License: Derivator © 2024 by William Alves Jardim is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/
+*
+*/
 var OperacoesModule = {
 
     /**Calcular a derivada usando o método númerico */
